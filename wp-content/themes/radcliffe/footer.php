19eb0b16beb9956febeb6a6d89b721daa597596e
Romain <romain.gazielly@gmail.com>
2016-04-24 01:45:04 +0200

    1st commit

---
+++ wp-content/themes/radcliffe/footer.php
@@ -0,0 +1,18 @@
+<div class="credits section small-padding">
+
+	<div class="credits-inner section-inner">
+	
+		<p class="fleft"></p>
+		
+		<p class="fright">Écris par une belle inconnue</p>
+		
+		<div class="clear"></div>
+		
+	</div> <!-- /credits-inner -->
+
+</div> <!-- /credits -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

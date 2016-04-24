4a05822bb261968458da3e664688ffcf45695ced
romaingazielly <romain.gazielly@gmail.com>
2016-04-24 02:33:42 +0200

    config

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'unicorn');
 
 /** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'nod');
 
 /** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
